37271aa742ec193b1f88723a681de0709660fdc7
icemilk00 <442377973@qq.com>
2016-10-09 17:49:13 +0800

    add code

---
+++ HPLiveSDK/livesession/AudioSource/HPAudioSource.h
@@ -0,0 +1,13 @@
+//
+//  HPAudioSource.h
+//  HPLiveSDKDemo
+//
+//  Created by hp on 16/10/9.
+//  Copyright © 2016年 51vv. All rights reserved.
+//
+
+#import "AEAudioController.h"
+
+@interface HPAudioSource : AEAudioController
+
+@end
---
+++ HPLiveSDK/livesession/AudioSource/HPAudioSource.m
@@ -0,0 +1,13 @@
+//
+//  HPAudioSource.m
+//  HPLiveSDKDemo
+//
+//  Created by hp on 16/10/9.
+//  Copyright © 2016年 51vv. All rights reserved.
+//
+
+#import "HPAudioSource.h"
+
+@implementation HPAudioSource
+
+@end
